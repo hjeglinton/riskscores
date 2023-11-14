d6f769c1762fc334c602a7e0a525633e2a9436d6
hjeglinton <124302914+hjeglinton@users.noreply.github.com>
2023-11-14 14:43:01 -0500

    Added functions from risk.R, vignette

---
+++ R/generic.R
@@ -0,0 +1,127 @@
+#' Summarize Risk Model Fits
+#'
+#' Prints text that summarizes risk_mod objects
+#' @param object an object of class "risk_mod", usually a result of a call to risk_mod()
+#' @return text with intercept, nonzero coefficients, gamma, lambda, and deviance
+#' @export
+summary.risk_mod <- function(object, ...) {
+
+  coef <- object$beta
+  res_metrics <- get_metrics(object)
+
+  cat("\nIntercept: ", coef[1], "\n", sep = "")
+  cat("\n")
+
+  nonzero_beta <- coef[coef != 0][-1] %>%
+    as.data.frame()
+  cat("Non-zero coefficients:")
+  stats::printCoefmat(nonzero_beta)
+  cat("\n")
+
+  cat("Gamma (multiplier): ", object$gamma, "\n")
+  cat("Lambda (regularizer): ", object$lambda0, "\n\n")
+  cat("Deviance: ", object$glm_mod$deviance, "\n")
+  cat("AIC: ", object$glm_mod$aic, "\n\n")
+
+}
+
+
+#' Extract Model Coefficients
+#'
+#' Extracts model coefficients (both nonzero and zero) from risk_mod object
+#' @param object an object of class "risk_mod", usually a result of a call to risk_mod()
+#' @return numeric vector with coefficients
+#' @export
+coef.risk_mod <- function(object) {
+
+  names(object$beta)[1] <- "(Intercept)"
+  object$beta
+
+}
+
+#' Model Predictions
+#'
+#' Obtains predictions from a risk score model object.
+#' @param object an object of class "risk_mod", usually a result of a call to
+#' risk_mod()
+#' @param newdata optionally, a data frame in which to look for variables with
+#' which to predict. If omitted, the fitted linear predictors are used.
+#' @param type the type of prediction required. The default is on the scale of
+#' the linear predictors; the alternative "response is on the scale of the
+#' response variable. Thus for a risk score model, the default predictions are of
+#' log-odds (probabilities on logit scale) and type = "response" gives the
+#' predicted risk probabilities. The "score" option returns integer risk scores.
+#' @return array with predictions
+#' @export
+predict.risk_mod <- function(object, newdata = NULL,
+                             type = c("link", "response", "score")) {
+
+  if (is.null(newdata)) {
+    X <- object$X
+  } else {
+    X <- newdata
+  }
+
+  v <- object$gamma * X %*% object$beta
+  v <- clip_exp_vals(v)
+  p <- exp(v)/(1+exp(v))
+
+  type = match.arg(type)
+
+  if (type == "link") {
+    return(v)
+  } else if (type == "response") {
+    return(p)
+  } else if (type == "score") {
+    return(X[,-1] %*% object$beta[-1])
+  }
+
+}
+
+#' Plot cross-validation results
+#'
+#' Plots the mean deviance for each lambda tested during cross-validation
+#' @param x an object of class "cv_risk_mod", usually a result of a call to
+#' cv_risk_mod()
+#' @return ggplot object
+#' @export
+plot.cv_risk_mod <- function(x, ...) {
+
+  # get mean/sd deviance of lambda_min
+  min_mean <- x$results$mean_dev[x$results$lambda0 == x$lambda_min]
+  min_sd <- x$results$sd_dev[x$results$lambda0 == x$lambda_min]
+
+  # define x axis breaks
+  lambda_grid <- log(x$results$lambda0)
+  nlambda <- length(lambda_grid)
+  nonzero_seq <- x$results$nonzero
+  if (nlambda > 25) {
+    new_n <- ceiling(nlambda/25)
+    lambda_grid <- lambda_grid[seq(1, nlambda, new_n)]
+    nonzero_seq[-seq(1, nlambda, new_n)] <- ""
+  }
+
+  # create plot
+  cv_plot <- ggplot2::ggplot(x$results, ggplot2::aes(x = log(lambda0), y = mean_dev)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_linerange(ggplot2::aes(ymin = mean_dev - sd_dev, ymax= mean_dev + sd_dev)) +
+    ggplot2::geom_point(ggplot2::aes(x = log(x$lambda_min), y = min_mean), color = "red") +
+    ggplot2::geom_linerange(ggplot2::aes(x = log(x$lambda_min), ymin = min_mean - min_sd,
+                       ymax= min_mean + min_sd), color = "red", inherit.aes = FALSE) +
+    ggplot2::geom_hline(yintercept = min_mean + min_sd, linetype = "dashed", color = "red") +
+
+    ggplot2::geom_text(ggplot2::aes(x = log(lambda0), label = nonzero_seq,
+                  y = (max(mean_dev) + max(sd_dev))*1.01),
+              size = 3, col = 'grey30') +
+
+    ggplot2::scale_x_continuous(breaks = lambda_grid, labels = round(lambda_grid, 1)) +
+    ggplot2::labs(x = "Log Lambda", y = "Deviance") +
+    ggplot2::theme_bw() +
+    ggplot2::theme(panel.grid.minor = ggplot2::element_blank(),
+          axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust=1))
+
+
+  return(cv_plot)
+}
+
+
---
+++ R/helpers.R
@@ -0,0 +1,93 @@
+#' Get metrics from beta and gamma
+#'
+#' Calculates deviance, accuracy, sensitivity, and specificity
+#' @param mod riskMod object
+#' @param X input matrix with dimensions n x p, must match dimensions of beta
+#' in mod (default NULL)
+#' @param y numeric vector for the response variable (binomial) of length n,
+#' (default NULL)
+#' @return list with deviance (dev), accuracy (acc), sensitivity (sens), and
+#' specificity (spec)
+get_metrics <- function(mod, X = NULL, y = NULL, weights = NULL){
+
+  # Check if new data
+  if (is.null(X)+is.null(y) == 1) stop("Must provide both X and y")
+  if (is.null(X) & is.null(y)){
+    X = mod$X
+    y = mod$y
+  }
+
+  # Check compatibility
+  if (nrow(X) != length(y)) stop("X and y must match in number of observations")
+  if (ncol(X) != length(mod$beta)) stop("X is incompatible with the model")
+  if (sum(! (y %in% c(0,1)))) stop("y must be 0/1 valued")
+
+  # Get predicted probs and classes
+  v <- mod$gamma * X %*% mod$beta
+  v <- clip_exp_vals(v)
+  p <- exp(v)/(1+exp(v))
+  pred <- ifelse(p>=0.5, 1, 0)
+
+  # Deviance
+  dev <- -2*sum(y*log(p)+(1-y)*log(1-p))
+
+  # Confusion matrix
+  tp <- sum(pred == 1 & y == 1)
+  tn <- sum(pred == 0 & y == 0)
+  fp <- sum(pred == 1 & y == 0)
+  fn <- sum(pred == 0 & y == 1)
+
+  # Accuracy values
+  acc <- (tp+tn)/(tp+tn+fp+fn)
+  sens <- tp/(tp+fn)
+  spec <- tn/(tn+fp)
+
+  return(list(dev = dev, acc=acc, sens=sens, spec=spec))
+}
+
+#' Assign stratified fold ids
+#'
+#' Returns a vector of fold ids with an equal percentage of samples for each class
+#' @param y numeric vector for the response variable (binomial)
+#' @param nfolds number of folds (default 10)
+#' @param seed An integer that is used as argument by the set.seed() for
+#' offsetting the random number generator. Default is to leave the random number
+#' generator alone.
+#' @return vector with the same length as y
+#' @export
+stratify_folds <- function(y, nfolds = 10, seed = NULL) {
+
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+
+  index_y0 <- which(y == 0)
+  index_y1 <- which(y == 1)
+  folds_y0 <- sample(rep(seq(nfolds), length = length(index_y0)))
+  folds_y1 <- sample(rep(seq(nfolds), length = length(index_y1)))
+
+  foldids <- rep(NA, length(y))
+  foldids[index_y0] <- folds_y0
+  foldids[index_y1] <- folds_y1
+
+  return(foldids)
+}
+
+#' Get risk from score
+#'
+#' Returns the risk probabilities for the provided score values
+#' @param object an object of class "risk_mod", usually a result of a call to
+#' risk_mod()
+#' @param score numeric vector with score value(s)
+#' @return numeric vector with the same length as `score`
+#' @export
+get_risk <- function(object, score) {
+
+  risk <- exp(object$gamma*(object$beta[[1]] + score))/
+    (1+exp(object$gamma*(object$beta[[1]] + score)))
+
+  return(risk)
+
+}
+
+
