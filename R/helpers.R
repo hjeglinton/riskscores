83d998b30b0a4395e843651465442e2f3146f3bf
hjeglinton <124302914+hjeglinton@users.noreply.github.com>
2023-12-08 09:34:46 -0500

    Add checks

--- R/helpers.R
+++ R/helpers.R
@@ -69,12 +69,22 @@ get_metrics <- function(mod, X = NULL, y = NULL, weights = NULL){
 #' @export
 stratify_folds <- function(y, nfolds = 10, seed = NULL) {
 
+  # Set seed
   if (!is.null(seed)) {
     set.seed(seed)
   }
 
-  index_y0 <- which(y == 0)
-  index_y1 <- which(y == 1)
+  # Check at least 3 folds
+  if (nfolds <= 3) stop("Must have more than 3 folds")
+
+  # Check that y is binomial
+  if (length(unique(y)) != 2) stop("'y' must be binomial")
+
+  # Create folds
+  y_vals <- unique(y)
+
+  index_y0 <- which(y == y_vals[1])
+  index_y1 <- which(y == y_vals[2])
   folds_y0 <- sample(rep(seq(nfolds), length = length(index_y0)))
   folds_y1 <- sample(rep(seq(nfolds), length = length(index_y1)))
 
@@ -89,7 +99,7 @@ stratify_folds <- function(y, nfolds = 10, seed = NULL) {
 #'
 #' Returns the risk probabilities for the provided score value(s).
 #' @param object An object of class "risk_mod", usually a result of a call to
-#' [risk_mod()].
+#'  [risk_mod()].
 #' @param score Numeric vector with score value(s).
 #' @return Numeric vector with the same length as `score`.
 #' @examples
@@ -101,6 +111,10 @@ stratify_folds <- function(y, nfolds = 10, seed = NULL) {
 #'
 #' @export
 get_risk <- function(object, score) {
+
+  # Check that object is "risk_mod"
+  if (!inherits(object, "risk_mod"))
+    stop("'object' must be of class 'risk_mod'")
 
   risk <- exp(object$gamma*(object$beta[[1]] + score))/
     (1+exp(object$gamma*(object$beta[[1]] + score)))
